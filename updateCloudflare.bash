98de36123c8604f2dd5f986ae8388c29bf86e7bc
Ian Chu <66397920+forkitupforkitup@users.noreply.github.com>
2022-08-17 13:44:51 -0400

    Update updateCloudflare.bash
    restart the service

--- updateCloudflare.bash
+++ updateCloudflare.bash
@@ -6,3 +6,5 @@ sudo rpm -ivh cloudflared-linux-x86_64.rpm;
 rm cloudflared-linux-x86_64.rpm;
 sudo cloudflared service uninstall;
 sudo cloudflared service install;
+sudo systemctl start cloudflared;
+systemctl status cloudflared;
